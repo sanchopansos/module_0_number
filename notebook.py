b8a09f50e679d2b0a0035868941ab399dd77d20b
sanchopansos <63538787+sanchopansos@users.noreply.github.com>
2020-05-20 20:46:56 +1000

    Update notebook.py

--- notebook.py
+++ notebook.py
@@ -1,2 +1,8 @@
-import number
-number.score_game(number.game_core())
+from number import game_core
+
+# Ask user to select a random number between 1 and 99.
+number = int(input('Загадайте число от 1 до 99:\n'))
+
+# Run the algorithm.
+attempts_count = game_core(number)
+print(f'Алгоритм угадал число за {attempts_count} попыток.')
